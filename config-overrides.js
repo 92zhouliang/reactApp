8aba6a033fe0096002f9320a26c9d9fab697b3da
liang.zhou <liang.zhou@mobilemd.cn>
2021-09-14 16:39:46 +0800

    feat:修改配置

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,17 @@
-const { override, fixBabelImports } = require("customize-cra");
+const { override, fixBabelImports, addDecoratorsLegacy, addWebpackAlias } = require("customize-cra");
+
+const path = require('path');
+
+// override 从左向右执行重写配置函数
 module.exports = override(
+  // alias
+  addWebpackAlias({
+    'src':path.resolve(__dirname,'src')
+  }),
   fixBabelImports("import", {
     libraryName: "antd-mobile",
     style: "css",
-  })
+  }),
+  // 添加支持装饰器语法 @babel/plugin-proposal-decorators
+  addDecoratorsLegacy()
 );
